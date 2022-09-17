9cf7aa077bd0b702701872c436adaa2d48530072
Pablo <40728995+CiclistaSinLicencia@users.noreply.github.com>
2022-09-16 21:33:30 -0300

    primer-upload-1.0

---
+++ script.js
@@ -0,0 +1,113 @@
+document.getElementById('dark-theme-btn').addEventListener
+('click' , ( ) => {
+    document.documentElement.style.setProperty('--background-theme' , '#333' )
+
+
+    document.documentElement.style.setProperty('--background-lighttheme-btn' , '#d3d3d3' )
+    document.documentElement.style.setProperty('--background-darktheme-btn' , '#75b418' )
+    document.documentElement.style.setProperty('--color-btn-darktheme' , '#272727' )
+    document.documentElement.style.setProperty('--background-darktheme-btn' , '#75b418' )
+
+
+    document.documentElement.style.setProperty('--header-background' , '#272727' )
+    document.documentElement.style.setProperty('--color-btn-nav' , '#ffffff' )
+    document.documentElement.style.setProperty('--background-color-octubre' , '#272727' )
+                     /*  */
+    document.documentElement.style.setProperty('--background-color-bloque-joyas' , '#272727' )
+    document.documentElement.style.setProperty('--background-color-bloque-joyas-hover' , ' #adff332f' )
+    document.documentElement.style.setProperty('--border-color--joyas' , 'gray' )
+    document.documentElement.style.setProperty('--color-texto-joyas' , 'floralwhite' )
+
+    document.documentElement.style.setProperty('--color-anchor-titulo-joyas' , ' #75b418' )
+    
+    
+   /* PADDING BOTON */
+   document.documentElement.style.setProperty('--padding-dark' , '8px' )
+   document.documentElement.style.setProperty('--padding' , '6px' )
+
+   
+})
+
+document.getElementById('light-theme-btn').addEventListener
+('click', ( ) => {
+    document.documentElement.style.setProperty('--background-theme', '#ffffff' )
+
+    document.documentElement.style.setProperty('--background-lighttheme-btn' , '#75b418' )
+    document.documentElement.style.setProperty('--background-darktheme-btn' , '#d3d3d3 ' )
+
+    document.documentElement.style.setProperty('--header-background' , '#ffffff' )
+    document.documentElement.style.setProperty('--color-btn-nav' , 'dark' )
+    document.documentElement.style.setProperty('--background-color-octubre' , '#272727' )
+                     /*  */
+    document.documentElement.style.setProperty('--background-color-bloque-joyas' , '#ececec' )
+    document.documentElement.style.setProperty('--border-color--joyas' , 'gray' )
+    document.documentElement.style.setProperty('--color-texto-joyas' , '#202020' )
+
+    document.documentElement.style.setProperty('--color-anchor-titulo-joyas' , ' darkgreen' )
+
+})
+
+   /* PADDING BOTON */
+   document.documentElement.style.setProperty('--padding-dark', '6px' )
+
+   document.documentElement.style.setProperty('--padding' , '8px' )
+
+
+
+/* BOTONES MOBILE */
+
+document.getElementById('dark-theme-mobile-btn').addEventListener
+('click' , ( ) => {
+    document.documentElement.style.setProperty('--background-theme' , '#333' )
+
+
+    document.documentElement.style.setProperty('--background-lighttheme-btn' , '#d3d3d3' )
+    document.documentElement.style.setProperty('--background-darktheme-btn' , '#75b418' )
+    document.documentElement.style.setProperty('--color-btn-darktheme' , '#272727' )
+    document.documentElement.style.setProperty('--background-darktheme-btn' , '#75b418' )
+
+
+    document.documentElement.style.setProperty('--header-background' , '#272727' )
+    document.documentElement.style.setProperty('--color-btn-nav' , '#ffffff' )
+    document.documentElement.style.setProperty('--background-color-octubre' , '#272727' )
+                     /*  */
+    document.documentElement.style.setProperty('--background-color-bloque-joyas' , '#272727' )
+    document.documentElement.style.setProperty('--background-color-bloque-joyas-hover' , ' #adff332f' )
+    document.documentElement.style.setProperty('--border-color--joyas' , 'gray' )
+    document.documentElement.style.setProperty('--color-texto-joyas' , 'floralwhite' )
+
+    document.documentElement.style.setProperty('--color-anchor-titulo-joyas' , ' #75b418' )
+   
+
+    /* DROPDOWN */
+    document.documentElement.style.setProperty('--mobile-dropdown-background' , ' #333' )
+    document.documentElement.style.setProperty('--mobile-dropdown-color' , ' white' )
+
+    
+    
+})
+
+document.getElementById('light-theme-mobile-btn').addEventListener
+('click', ( ) => {
+    document.documentElement.style.setProperty('--background-theme', '#ffffff' )
+
+    document.documentElement.style.setProperty('--background-lighttheme-btn' , '#75b418' )
+    document.documentElement.style.setProperty('--background-darktheme-btn' , '#d3d3d3 ' )
+
+    document.documentElement.style.setProperty('--header-background' , '#ffffff' )
+    document.documentElement.style.setProperty('--color-btn-nav' , 'dark' )
+    document.documentElement.style.setProperty('--background-color-octubre' , '#272727' )
+                     /*  */
+    document.documentElement.style.setProperty('--background-color-bloque-joyas' , '#ececec' )
+    document.documentElement.style.setProperty('--border-color--joyas' , 'gray' )
+    document.documentElement.style.setProperty('--color-texto-joyas' , '#202020' )
+
+    document.documentElement.style.setProperty('--color-anchor-titulo-joyas' , ' darkgreen' )
+
+    /* DROPDOWN */
+    document.documentElement.style.setProperty('--mobile-dropdown-background' , ' white' )
+    document.documentElement.style.setProperty('--mobile-dropdown-color' , ' #272727' )
+})
+
+/* windows.getComputedStyle(document.documentElement)
+.getPropertyValue('--dark-theme') */
