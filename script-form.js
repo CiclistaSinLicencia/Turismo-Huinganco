0ab527401ca84391307007de4cd8a69e179409ec
Pablo <p4blo.kuhn@gmail.com>
2022-09-27 22:07:44 -0300

    form validation

--- script-form.js
+++ script-form.js
@@ -1,72 +1,36 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+function validation()
+{
+                var form = document.getElementById("form-news");
+                var email = document.getElementById("email-news").value;
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
 
-    validateInputs();
-});
+                var submit = document.getElementById("submit-news");
 
-const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
+                 var text = document.getElementById("text"); 
+                var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success')
+                if (email.match(pattern))
+                {
+                    form.classList.add("valid");
+                    form.classList.remove("invalid");
+                    text.innerHTML = "eMail valido";
+                    text.style.color = "#b8ff5c"
+                    submit.style.backgroundColor = " #75b418";
+                }
+                else
+                {
+                    form.classList.remove("valid");
+                    form.classList.add("invalid");
+                    text.innerHTML = "el eMail es Invalido";
+                    text.style.color = "#fa7373";
+                    submit.style.backgroundColor = " #fa7373";
+                }
+                if (email == "")
+                {
+                    form.classList.remove("valid");
+                    form.classList.remove("invalid");
+                    text.innerHTML = "";
+                    text.style.color = "#b8ff5c"
+                    submit.style.backgroundColor = " #75b418";
+                }
 }
-
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-};
-
-const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
-
-    if(usernameValue === '') {
-        setError(username, 'Username is required');
-    } else {
-        setSuccess(username);
-    }
-
-    if(emailValue === '') {
-        setError(email, 'Email is required');
-    } else if (!isValidEmail(emailValue)) {
-        setError(email, 'Provide a valid email address');
-    } else {
-        setSuccess(email);
-    }
-
-    if(passwordValue === '') {
-        setError(password, 'Password is required');
-    } else if (passwordValue.length < 8 ) {
-        setError(password, 'Password must be at least 8 character.')
-    } else {
-        setSuccess(password);
-    }
-
-    if(password2Value === '') {
-        setError(password2, 'Please confirm your password');
-    } else if (password2Value !== passwordValue) {
-        setError(password2, "Passwords doesn't match");
-    } else {
-        setSuccess(password2);
-    }
-
-};
