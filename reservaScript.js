fba1e7f789e6ed9be328c2d6a588354bd5d1395e
Pablo <p4blo.kuhn@gmail.com>
2022-10-23 16:35:23 -0300

    cambio form huin, mejora colores joyas

--- reservaScript.js
+++ reservaScript.js
@@ -15,59 +15,21 @@ function funCinco(){
     montoPagar = 25
     console.log(montoPagar)
     totalPagar.textContent =  "El monto total a pagar es $" + montoPagar
-    funDescuento()
 }
 
 function funDiez(){
     montoPagar = 85
     console.log(montoPagar)
     totalPagar.textContent = "El monto total a pagar es $" + montoPagar
-    funDescuento()
 }
 
 function funveintiCinco(){
     montoPagar = 125
     console.log(montoPagar)
     totalPagar.textContent = "El monto total a pagar es $" + montoPagar
-    funDescuento()
 }
 function funCincuenta(){
     montoPagar = 199
     console.log(montoPagar)
     totalPagar.textContent = "El monto total a pagar es $" + montoPagar
-    funDescuento()
-}
-
-
-function funDescuento(){
-    
-document.addEventListener('input', function (event) {
-
-	// Only run on our select menu
-	if (event.target.id !== 'pais-form-reserva') return;
-    
-    if (event.target.value === 'argentina') {
-		descuento.textContent = "Descuento 30% residente " + (montoPagar * 30 / 100 + montoPagar)
-        console.log("Descuento 30% residente " + (montoPagar * 30 / 100 + montoPagar))
-	}
-    if (event.target.value === 'chile') {
-		descuento.textContent = "tenes que pagar 180% mas " + (montoPagar * 180 / 100 + montoPagar)
-        console.log("Descuento 30% residente " + (montoPagar * 30 / 100 + montoPagar))
-	}
-    if (event.target.value === 'brasil') {
-		descuento.textContent = "tenes que pagar 180% mas " + (montoPagar * 180 / 100 + montoPagar)
-        console.log("Descuento 30% residente " + (montoPagar * 30 / 100 + montoPagar))
-	}
-    if (event.target.value === 'paraguay') {
-		descuento.textContent = "tenes que pagar 180% mas " + (montoPagar * 180 / 100  + montoPagar)
-        console.log("Descuento 30% residente " + (montoPagar * 30 / 100 + montoPagar))
-	}
-	// The selected value
-	console.log(event.target.value);
-
-	// The selected option element
-	console.log(event.target.options[event.target.selectedIndex]);
-
-}, false);
-
 }
