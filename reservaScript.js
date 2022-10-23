45553bd31c62e834d6ff9fdc6c27457bee6ecde4
Pablo <p4blo.kuhn@gmail.com>
2022-10-23 02:16:56 -0300

    javamuestra de precios

---
+++ reservaScript.js
@@ -0,0 +1,31 @@
+let cinco = document.getElementById("5km").addEventListener("change" ,funCinco)
+let  diez = document.getElementById("10km").addEventListener("change" ,funDiez)
+let  veintiCinco = document.getElementById("25km").addEventListener("change" ,funveintiCinco)
+let  cincuenta = document.getElementById("50km").addEventListener("change", funCincuenta)
+
+let totalPagar = document.getElementById("totalPagar")
+
+let montoPagar = 0
+
+function funCinco(){
+    montoPagar = 25
+    console.log(montoPagar)
+    totalPagar.innerHTML = "El monto total a pagar es $" + montoPagar
+}
+
+function funDiez(){
+    montoPagar = 85
+    console.log(montoPagar)
+    totalPagar.innerHTML = "El monto total a pagar  es $" + montoPagar
+}
+
+function funveintiCinco(){
+    montoPagar = 125
+    console.log(montoPagar)
+    totalPagar.innerHTML = "El monto total a pagar  es $" + montoPagar
+}
+function funCincuenta(){
+    montoPagar = 200
+    console.log(montoPagar)
+    totalPagar.innerHTML = "El monto total a pagar  es $" + montoPagar
+}
