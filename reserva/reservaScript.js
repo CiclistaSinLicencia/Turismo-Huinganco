eb6f3ff177bc5df122c78b2373a724a5cc4481c1
Pablo <p4blo.kuhn@gmail.com>
2022-10-29 12:58:42 -0300

    CssUpdate
    *se unificaron los themes en style
    *ahora el formulario de mail funciona correctamente
    *se arreglo el autofill del formulario
    buggs:el mail form no quita el outline rojo
    
    Co-Authored-By: Pablo <40728995+CiclistaSinLicencia@users.noreply.github.com>

--- reserva/reservaScript.js
+++ reserva/reservaScript.js
@@ -5,119 +5,110 @@
 let cinco = document.getElementById("5km")
 cinco.addEventListener("change", five)
 
- let  diez = document.getElementById("10km")
+let diez = document.getElementById("10km")
 diez.addEventListener("change", ten)
 
-let  veintiCinco = document.getElementById("25km")
-veintiCinco.addEventListener("change",twentyFive)
+let veintiCinco = document.getElementById("25km")
+veintiCinco.addEventListener("change", twentyFive)
 
-let  cincuenta = document.getElementById("50km")
-cincuenta.addEventListener("change",fifty)  
+let cincuenta = document.getElementById("50km")
+cincuenta.addEventListener("change", fifty)
 
 let txtTotalPagar = document.getElementById("txtTotalPagar")
 
 let montoPagar
 /* posicion inicial */
-let paisOrigen = "Argentina" 
+let paisOrigen = "Argentina"
 let km = 0
 /*  */
 
 /* funciones resumidas */
 
-function funPaisOrigen(porcentajeDto,porcentajeSuma){
+function funPaisOrigen(porcentajeDto, porcentajeSuma) {
 
-    if(paisOrigen === "Argentina"){
-    
-            montoPagar = montoPagar * -porcentajeDto  /100 + montoPagar
-            console.log("Dto por ser Argentino " + montoPagar)
-             msjDto = " + dto 30% = " 
+    if (paisOrigen === "Argentina") {
+
+        montoPagar = montoPagar * -porcentajeDto / 100 + montoPagar
+        console.log("Dto por ser Argentino " + montoPagar)
+        msjDto = " + dto 30% = "
     }
-    if(paisOrigen === "Chile")
-    {
-            montoPagar = montoPagar * porcentajeSuma / 100  + montoPagar
-            msjDto = " + 180% x chileno = "    
+    if (paisOrigen === "Chile") {
+        montoPagar = montoPagar * porcentajeSuma / 100 + montoPagar
+        msjDto = " + 180% x chileno = "
     }
-    if(paisOrigen === "Otro")
-    {
-            montoPagar = montoPagar
-            msjDto = " "    
+    if (paisOrigen === "Otro") {
+        montoPagar = montoPagar
+        msjDto = " "
     }
-    if(dtoTrue === true){
+    if (dtoTrue === true) {
         montoPagar = montoPagar - 2
     }
 }
 
-function txtMontoPagar(){
-    txtTotalPagar.textContent = "Total "+ msjDto +  montoPagar + " Usd"
+function txtMontoPagar() {
+    txtTotalPagar.textContent = "Total " + msjDto + montoPagar + " Usd"
 }
 
 
-function calcKm (){
-   
-    if(km===1)
-    {
+function calcKm() {
+
+    if (km === 1) {
         five()
     }
-    if(km===2)
-    {
+    if (km === 2) {
         ten()
     }
-    if(km===3)
-    {
+    if (km === 3) {
         twentyFive()
     }
-    if(km===4)
-    {
+    if (km === 4) {
         fifty()
     }
 }
 
 /* select pais */
-document.getElementsByTagName('select')[0].onchange = function() {
+document.getElementsByTagName('select')[0].onchange = function () {
     var index = this.selectedIndex;
     var inputText = this.children[index].innerHTML.trim();
     paisOrigen = inputText
     calcKm()
-  }
+}
 
 /* 1 */
-function five(){   
- montoPagar = 25
+function five() {
+    montoPagar = 25
     km = 1
-    funPaisOrigen(30,180)
+    funPaisOrigen(30, 180)
     txtMontoPagar()
 }
 /* 2 */
-function ten()
-{
-    montoPagar =  85
+function ten() {
+    montoPagar = 85
     km = 2
-    funPaisOrigen(30,180)
+    funPaisOrigen(30, 180)
     txtMontoPagar()
 }
 /* 3 */
-function twentyFive()
-{
-    montoPagar =  125
+function twentyFive() {
+    montoPagar = 125
     km = 3
-    funPaisOrigen(30,180)
+    funPaisOrigen(30, 180)
     txtMontoPagar()
 }
 /* 4 */
-function fifty()
-{
-    montoPagar =  199
+function fifty() {
+    montoPagar = 199
     km = 4
-    funPaisOrigen(30,180)
+    funPaisOrigen(30, 180)
     txtMontoPagar()
 }
 
 
-let inputDescuento = document.getElementById ("cod-descuento-reserva")
+let inputDescuento = document.getElementById("cod-descuento-reserva")
 
 let txtCodDescuento = document.getElementById("txt-cod-descuento")
 
-inputDescuento.addEventListener("input",funDescuento)
+inputDescuento.addEventListener("input", funDescuento)
 /* descuento */
 
 
@@ -125,30 +116,30 @@ inputDescuento.addEventListener("input",funDescuento)
 
 let dtoCodigo = 0
 let dtoTrue = false
-function funDescuento(){
-    let inputValue= inputDescuento.value
- 
-    if(inputValue === "Ciclista" || inputValue === "ciclista"){
- 
+function funDescuento() {
+    let inputValue = inputDescuento.value
+
+    if (inputValue === "Ciclista" || inputValue === "ciclista") {
+
         inputDescuento.style.border = "3px solid var(--form-valid-border)"
         dtoTrue = true
         dtoCodigo = -2
-        txtCodDescuento.innerHTML = "tenes 2 Usd de descuento aplicado!" 
+        txtCodDescuento.innerHTML = "tenes 2 Usd de descuento aplicado!"
         txtCodDescuento.style.color = " var(--form-valid-txt) "
         calcKm()
     }
-    else if(inputValue=== ""){
-        
+    else if (inputValue === "") {
+
         dtoTrue = false
-        dtoCodigo = 0 
+        dtoCodigo = 0
         txtCodDescuento.innerHTML = "metele 'ciclista' para el dto "
         inputDescuento.style.border = "3px solid var( --form-input-border)"
-        calcKm() 
+        calcKm()
 
     }
-    else{  
+    else {
         dtoTrue = false
-        dtoCodigo = 0 
+        dtoCodigo = 0
         txtCodDescuento.innerHTML = " No es un codigo valido"
         inputDescuento.style.border = "3px solid #ff629483"
         txtCodDescuento.style.color = "#ff6294"
@@ -157,55 +148,47 @@ function funDescuento(){
 }
 
 
-/*  */
+
 
 /* FORMULARIO CONSULTA */
 
-       
-                 let emailReserva = document.getElementById("mail-form-reserva");  
-                 emailReserva.addEventListener("input", validationReserva)
-
-                 let formReserva = document.getElementById("form-Reserva");
-
-                let btnReserva = document.getElementById("btn-submit-reserva")
+let emailReserva = document.getElementById("mail-form-reserva");
+emailReserva.addEventListener("input", validationReserva)
+let formReserva = document.getElementById("form-Reserva");
+let btnReserva = document.getElementById("btn-submit-reserva")
 
 
-                 let patternConsul = /^[^ ]+@[^ ]+\.[a-z]{1,3}$/;
+let patternConsul = /^[^ ]+@[^ ]+\.[a-z]{1,3}$/;
 
-                function validationReserva()
-                {
-                    mailValue = emailReserva.value
-                    
-                  
-                if 
-                (mailValue.match(patternConsul))
-                {
-                    formReserva.classList.add("valid");
-                    formReserva.classList.remove("invalid");
+function validationReserva() {
+    mailValue = emailReserva.value
 
-                    btnReserva.style.backgroundColor = "#75b418"
-                    emailReserva.style.outline = "2px solid #75b418";
-                    console.log("FUNCIONA!")
-                }
-                  
-                if(!mailValue.match(patternConsul))
-                {
-                    formReserva.classList.remove("valid");
-                    formReserva.classList.remove("invalid");
+    if
+        (mailValue.match(patternConsul)) {
+        emailReserva.classList.add("valid");
+        emailReserva.classList.remove("invalid");
 
-                    btnReserva.style.backgroundColor = "#fa7373"
-                    emailReserva.style.outline = "2px solid #fa7373";
-                } 
-             
-                if (mailValue === "")
-                {
-                    formReserva.classList.remove("valid");
-                    formReserva.classList.add("invalid");
-                    emailReserva.style.outline = "0px;" 
-                    emailReserva.style.border = "0.5px solid var(--form-input-border)"; 
-                }
+        btnReserva.style.backgroundColor = "#75b418"
+        emailReserva.style.outline = "2px solid #75b418";
+        console.log("FUNCIONA!")
+    }
 
-              
+    if (!mailValue.match(patternConsul)) {
+        emailReserva.classList.remove("valid");
+        emailReserva.classList.remove("invalid");
+
+        btnReserva.style.backgroundColor = "#fa7373"
+        emailReserva.style.outline = "2px solid #fa7373";
+    }
+
+    if (mailValue === "") {
+        emailReserva.classList.remove("valid");
+        emailReserva.classList.add("invalid");
+        emailReserva.style.outline = "0px;"
+        emailReserva.style.border = "0.5px solid var(--form-input-border)";
+    }
+
+
 }
 
 
--- script.js
+++ script.js
@@ -1,158 +1,134 @@
-
 // Helper function
 let domReady = (cb) => {
-    document.readyState === 'interactive' || document.readyState === 'complete'
-      ? cb()
-      : document.addEventListener('DOMContentLoaded', cb);
-  };
-  
-  domReady(() => {
-    // Display body when DOM is loaded
-    document.body.style.visibility = 'visible';
-  });
+  document.readyState === "interactive" || document.readyState === "complete"
+    ? cb()
+    : document.addEventListener("DOMContentLoaded", cb);
+};
+
+domReady(() => {
+  // Display body when DOM is loaded
+  document.body.style.visibility = "visible";
+});
 
 //Themes
-let theme = localStorage.getItem('data-theme');
+let theme = localStorage.getItem("data-theme");
 
 const changeThemeToDark = () => {
-    document.documentElement.setAttribute("data-theme", "dark") 
-    localStorage.setItem("data-theme", "dark") 
-}
+  document.documentElement.setAttribute("data-theme", "dark");
+  localStorage.setItem("data-theme", "dark");
+};
 const changeThemeToLight = () => {
-    document.documentElement.setAttribute("data-theme", "light") 
-    localStorage.setItem("data-theme", 'light') 
-}
+  document.documentElement.setAttribute("data-theme", "light");
+  localStorage.setItem("data-theme", "light");
+};
 
-if(theme === 'dark'){
-    changeThemeToDark()
+if (theme === "dark") {
+  changeThemeToDark();
 }
 
 const checkbox = document.getElementById("theme");
-checkbox.addEventListener('change', () => {
-    let theme = localStorage.getItem('data-theme'); 
-    if (theme ==='dark'){
-        changeThemeToLight()
-    }else{
-        changeThemeToDark()
-    }   
+checkbox.addEventListener("change", () => {
+  let theme = localStorage.getItem("data-theme");
+  if (theme === "dark") {
+    changeThemeToLight();
+  } else {
+    changeThemeToDark();
+  }
 });
 
 //btnSound
 const audio = new Audio();
 audio.src = "/audio/click.wav";
 
-
 /* ------------------------------END THEME------------------------------ */
 
-
 /* IMAGENES Carousel */
-const buttons = document.querySelectorAll("[data-carousel-button]")
-buttons.forEach(button => {
-    button.addEventListener("click", ( ) => {
-            const offset = button.dataset.carouselButton === "next" ? 1 : -1
-            const slides = button
-            .closest("[data-carousel]")
-            .querySelector("[data-slides]")
+const buttons = document.querySelectorAll("[data-carousel-button]");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
 
-            const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset
-        if (newIndex < 0) newIndex = slides.children.length -1
-        if (newIndex >= slides.children.length) newIndex = 0
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
 
-        slides.children[newIndex].dataset.active = true
-        delete activeSlide.dataset.active
-    })
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
 });
 
 /* ANIMACION boton dropdown */
 $(document).ready(function () {
-    $(".burger").on("click", function () {
-      $(this).toggleClass("active");
-    });
+  $(".burger").on("click", function () {
+    $(this).toggleClass("active");
   });
+});
 
-  /* FORMULARIO NEWS */
-function validation()
-{
-                let form = document.getElementById("form-news");
-                let email = document.getElementById("email-news").value;
-                let submit = document.getElementById("submit-news");
-                 let text = document.getElementById("text"); 
-                let pattern = /^[^ ]+@[^ ]+\.[a-z]{1,3}$/;
+/* FORMULARIO NEWS */
+function validation() {
+  let form = document.getElementById("form-news");
+  let email = document.getElementById("email-news").value;
+  let submit = document.getElementById("submit-news");
+  let text = document.getElementById("text");
+  let pattern = /^[^ ]+@[^ ]+\.[a-z]{1,3}$/;
 
-                if (email.match(pattern))
-                {
-                    form.classList.add("valid");
-                    form.classList.remove("invalid");
-                    text.innerHTML = "eMail valido";
-                    text.style.color = "#b8ff5c"
-                    submit.style.backgroundColor = " #75b418";
-                }
-                else
-                {
-                    form.classList.remove("valid");
-                    form.classList.add("invalid");
-                    text.innerHTML = "el eMail es Invalido";
-                    text.style.color = "#fa7373";
-                    submit.style.backgroundColor = "#fa7373";
-                    
-                }
-                if (email == "")
-                {
-                    form.classList.remove("valid");
-                    form.classList.remove("invalid");
-                    text.innerHTML = "";
-                    text.style.color = "#b8ff5c"
-                    submit.style.backgroundColor = " #2596be";
-                }
+  if (email.match(pattern)) {
+    form.classList.add("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "eMail valido";
+    text.style.color = "#b8ff5c";
+    submit.style.backgroundColor = " #75b418";
+  } else {
+    form.classList.remove("valid");
+    form.classList.add("invalid");
+    text.innerHTML = "el eMail es Invalido";
+    text.style.color = "#fa7373";
+    submit.style.backgroundColor = "#fa7373";
+  }
+  if (email == "") {
+    form.classList.remove("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "";
+    text.style.color = "#b8ff5c";
+    submit.style.backgroundColor = " #2596be";
+  }
 }
 /* FORMULARIO CONSULTA */
-function validationConsul()
-{
-                let formConsul = document.getElementById("form-consul");
-                 let emailConsul = document.getElementById("email-consul").value;  
-                let emailConsul2 = document.getElementById("email-consul");
-                let btnConsul = document.getElementById("btn-submit-consul");
-             
-                let patternConsul = /^[^ ]+@[^ ]+\.[a-z]{1,3}$/;
+function validationConsul() {
+  let formConsul = document.getElementById("form-consul");
+  let emailConsul = document.getElementById("email-consul").value;
+  let emailConsul2 = document.getElementById("email-consul");
+  let btnConsul = document.getElementById("btn-submit-consul");
 
-                if 
-                (emailConsul.match(patternConsul))
-                {
-                    formConsul.classList.add("valid");
-                    formConsul.classList.remove("invalid");
-                    
-                    emailConsul2.style.border = "0px";
-                    emailConsul2.style.outline = "2px solid #75b418"
-                    btnConsul.style.backgroundColor = " #75b418";;
-                }
-                else
-                {
-                    formConsul.classList.remove("valid");
-                    formConsul.classList.add("invalid");
-                  
-                    emailConsul2.style.border = "0px";
-                    emailConsul2.style.outline = "2px solid #fa7373";
-                    btnConsul.style.backgroundColor = " #fa7373";
-                }
-                if (emailConsul == "")
-                {
-                    formConsul.classList.remove("valid");
-                    formConsul.classList.remove("invalid");
-               
-                    emailConsul2.style.backgroundColor = "var(--form-input-backG)";
-                    emailConsul2.style.border = "0px";
-                    emailConsul2.style.outline = "1px solid var(--form-input-border)";
+  let patternConsul = /^[^ ]+@[^ ]+\.[a-z]{1,3}$/;
 
-                    btnConsul.style.backgroundColor = " #75b418";;
-                 
-                }
+  if (emailConsul.match(patternConsul)) {
+    formConsul.classList.add("valid");
+    formConsul.classList.remove("invalid");
+
+    emailConsul2.style.border = "0px";
+    emailConsul2.style.outline = "2px solid #75b418";
+    btnConsul.style.backgroundColor = " #75b418";
+  } else {
+    formConsul.classList.remove("valid");
+    formConsul.classList.add("invalid");
+
+    emailConsul2.style.border = "0px";
+    emailConsul2.style.outline = "2px solid #fa7373";
+    btnConsul.style.backgroundColor = " #fa7373";
+  }
+  if (emailConsul == "") {
+    formConsul.classList.remove("valid");
+    formConsul.classList.remove("invalid");
+
+    emailConsul2.style.backgroundColor = "var(--form-input-backG)";
+    emailConsul2.style.border = "0px";
+    emailConsul2.style.outline = "1px solid var(--form-input-border)";
+
+    btnConsul.style.backgroundColor = " #75b418";
+  }
 }
-
-
-
-
-
-
-
-
